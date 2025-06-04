c28f2a941c58ad5da6f05ab5fd1f07d8066e978e
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-04 14:51:16 +1000

    -Implemented predicate example

--- src/functionalinterfaces/PredicateExample.java
+++ src/functionalinterfaces/PredicateExample.java
@@ -1,5 +1,15 @@
 package functionalinterfaces;
 
-public class PredicateExample {
+import java.util.function.Predicate;
 
+public class PredicateExample {
+    public static void main(String[] args) {
+        Predicate<Integer> predicate = (i) -> i > 10;
+
+        Predicate<Integer> isEven = (i) -> i % 2 == 0;
+
+        System.out.println("Is 15 greater than 10 and even as well ? " + predicate.and(isEven).test(15)); // true
+        System.out.println("Is 15 greater than 10 or even as well ? " + predicate.or(isEven).test(15)); // false
+        System.out.println("Is 15 not greater than 10 ? " + predicate.negate().test(15)); // false
+    }
 }
