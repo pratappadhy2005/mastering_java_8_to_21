46205e62f82171110039dfedc907bad9271e0d8a
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-09 11:44:42 +1000

    -Refactored method ref using consumer fucntional interface

--- src/method/references/ConsumerMethodRefExample.java
+++ src/method/references/ConsumerMethodRefExample.java
@@ -10,10 +10,15 @@ public class ConsumerMethodRefExample {
     static Consumer<Student> studentConsumerLambda = (student) -> System.out.println(student);
     static Consumer<Student> studentConsumerMethodRef = System.out::println;
 
+    static Consumer<Student> studentConsumer = Student::printActivities;
+
 
     public static void main(String[] args) {
         StudentDataBase.getAllStudents().forEach(studentConsumerLambda);
         System.out.println("===================================");
         StudentDataBase.getAllStudents().forEach(studentConsumerMethodRef);
+        System.out.println("===================================");
+        StudentDataBase.getAllStudents().stream()
+                .forEach(student -> studentConsumer.accept(student));
     }
 }
--- src/method/references/FunctionMethodReference.java
+++ src/method/references/FunctionMethodReference.java
@@ -11,14 +11,8 @@ public class FunctionMethodReference {
     static Function<String, String> toUpperCaseLambda = (str) -> str.toUpperCase();
     static Function<String, String> toUpperCaseMethodRef = String::toUpperCase;
 
-    static Consumer<Student> studentConsumer = Student::printActivities;
-
-
-
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         System.out.println(toUpperCaseLambda.apply("hello world"));
         System.out.println(toUpperCaseMethodRef.apply("hello world"));
-        StudentDataBase.getAllStudents().stream()
-                .forEach(student -> studentConsumer.accept(student));
     }
 }
