b9070c6634cd7118dac3762e43c941ea4d7dff33
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-14 19:08:18 +1000

    -Debugging streams using peek example

--- src/streams/StreamsExample.java
+++ src/streams/StreamsExample.java
@@ -14,9 +14,17 @@ public class StreamsExample {
 
         // student name and their activities
         Map<String, List<String>> studentMap = StudentDataBase.getAllStudents()
-                //.stream()
-                .parallelStream()
+                .stream()
+                .peek((student) -> {
+                    // This is just for demonstration; it will print each student before filtering
+                    System.out.println("Student: " + student);
+                })
+                //.parallelStream()
                 .filter(studentPredicate) // Filter students in grade 3 or higher
+                .peek((student) -> {
+                    // This is just for demonstration; it will print each student after filtering
+                    System.out.println("Filtered Student: " + student);
+                })
                 .collect(Collectors.toMap(
                         Student::getName,
                         Student::getActivities
