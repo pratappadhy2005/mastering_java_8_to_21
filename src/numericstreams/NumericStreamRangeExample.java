652eedef9a2c0a008b0d41c42d04776ff2674e38
Pratap Padhy <pratappadhy2005@gmail.com>
2025-07-13 21:41:00 +1000

    -Added int and long stream

--- src/numericstreams/NumericStreamRangeExample.java
+++ src/numericstreams/NumericStreamRangeExample.java
@@ -17,5 +17,9 @@ public class NumericStreamRangeExample {
         LongStream.range(1, 10).forEach(System.out::println); // Generates a stream of long integers from 1 to 9
         System.out.println("-----");
         LongStream.rangeClosed(1, 10).forEach(System.out::println); // Generates a stream of long integers from 1 to 10
+
+        // Example of using range with a step
+        System.out.println("-----");
+        IntStream.range(1, 20).filter(n -> n % 2 == 0).forEach(System.out::println); // Prints even numbers from 1 to 19
     }
 }
