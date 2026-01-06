140ee9be862a6156a12ec0086cf7683630076be0
Pratap Padhy <pratappadhy2005@gmail.com>
2026-01-07 10:00:28 +1100

    -Added comprator example using traditional vs lambda way

---
+++ src/revisit/lambdas/ComparatorLambdaExample.java
@@ -0,0 +1,25 @@
+package revisit.lambdas;
+
+import java.util.Comparator;
+
+public class ComparatorLambdaExample {
+    public static void main(String[] args) {
+        /**
+         * Prior to Java 8
+         */
+        Comparator<Integer> comparatorOld = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+
+        System.out.println("Comparison using anonymous class: " + comparatorOld.compare(10, 20));
+
+        /**
+         * Java 8 Lambda Expression
+         */
+        Comparator<Integer> comparatorLambda = (o1, o2) -> o1.compareTo(o2);
+        System.out.println("Comparison using lambda expression: " + comparatorLambda.compare(10, 20));
+    }
+}
