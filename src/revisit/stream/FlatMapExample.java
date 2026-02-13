11b818f33aac60d4dd16a0648b72ece1868bee2b
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-14 09:25:50 +1100

    -Added sort, count and distinct

--- src/revisit/stream/FlatMapExample.java
+++ src/revisit/stream/FlatMapExample.java
@@ -7,11 +7,19 @@ import java.util.stream.Collectors;
 
 public class FlatMapExample {
 
+    public Long getStudentActivitiesCount() {
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .flatMap(student -> student.getActivities().stream())
+                .distinct()
+                .count();
+    }
     public List<String> getStudentActivities() {
         return StudentDataBase.getAllStudents()
                 .stream()
                 .flatMap(student -> student.getActivities().stream())
                 .distinct()
+                .sorted()
                 .collect(Collectors.toList());
     }
 
@@ -19,5 +27,8 @@ public class FlatMapExample {
         FlatMapExample flatMapExample = new FlatMapExample();
         List<String> studentActivities = flatMapExample.getStudentActivities();
         System.out.println(studentActivities);
+
+        System.out.println("Total number of activities: ");
+        System.out.println(flatMapExample.getStudentActivitiesCount());
     }
 }
