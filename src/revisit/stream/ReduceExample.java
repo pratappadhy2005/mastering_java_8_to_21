4023da8d28b6e4ce71f8ad1e978790bbcfcea475
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-19 07:34:06 +1100

    -Added reduce part 2 example

--- src/revisit/stream/ReduceExample.java
+++ src/revisit/stream/ReduceExample.java
@@ -1,5 +1,8 @@
 package revisit.stream;
 
+import revisit.data.Student;
+import revisit.data.StudentDataBase;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -24,11 +27,27 @@ public class ReduceExample {
                 .reduce((a,b) -> a*b);
     }
 
+    public static Optional<Student> getHighestGpaStudent(List<Student> studentList) {
+        return studentList.stream()
+                .reduce((s1, s2) -> s1.getGpa() > s2.getGpa() ? s1 : s2);
+    }
+
     public static void main(String[] args) {
         List<Integer> integerList = List.of(1, 3, 5, 7);
         System.out.println(performMultiplication(integerList));
+
+        System.out.println("********************");
+
         System.out.println(performMultiplicationV2(integerList));
 
+        System.out.println("********************");
+
         System.out.println(performMultiplicationWithoutIdentity(integerList).orElse(0));
+
+        System.out.println("********************");
+
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        Optional<Student> studentOptional = getHighestGpaStudent(studentList);
+        studentOptional.ifPresent(System.out::println);
     }
 }
